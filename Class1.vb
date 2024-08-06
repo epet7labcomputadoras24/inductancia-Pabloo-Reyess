26075ea1ae265efc0bc3f2a39e1908e0ce7ac1f0
Pabloo-Reyess <pabloreyes290606@gmail.com>
2024-08-06 20:03:11 -0300

    Tarea completado1

--- Class1.vb
+++ Class1.vb
@@ -1,5 +1,15 @@
-﻿Imports Microsoft.VisualBasic
+﻿Imports System
 
-Public Class Class1
-
-End Class
+Module Program
+    Sub Main()
+        Dim xl As Double
+        Dim l As Double
+        Dim f As Double
+        Console.WriteLine("Ingrese el valor de la inductancia:")
+        l = Console.ReadLine()
+        Console.WriteLine("Ingrese el valor de la frecuencia:")
+        f = Console.ReadLine()
+        xl = (l * (2 * 3.1415) * f)
+        Console.WriteLine("Tu Xl es de:" & xl)
+    End Sub
+End Module
